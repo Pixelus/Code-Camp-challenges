091b6b1e7bc0b707c3854e2aba6af0f418e3305b
Pixelus <s.malafosse@gmail.com>
2019-02-05 18:18:59 +0100

    Create temperature.js

---
+++ Basic Algorithm Scripting/temperature.js
@@ -0,0 +1,9 @@
+// Program to convert a Fahrenheit temperature in Celsius.
+// Return the Celsius temperature equivalent to the given Fahrenheit temperature. 
+
+function convertToF(celsius) {
+  let fahrenheit = (celsius * 9/5) + 32;
+  return fahrenheit;
+}
+
+convertToF(30);
